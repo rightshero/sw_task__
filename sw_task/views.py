7e88c05b7337cc356723e76a79cf91676f857b5b
auto <auto>
2023-10-12 19:08:49 +0300

    add delete feature (end of the backend requirements)

--- sw_task/views.py
+++ sw_task/views.py
@@ -12,10 +12,27 @@ import collections
 
 class HomeView(View):
     def get(self,request):
+        """
+            Summary:
+                This view returns an adjacency list to construct a tree.
+
+            Args:
+                request: A Django HTTP request object.
+            Returns:
+                dict: An adjacency list (e.g., a dictionary) representing the tree structure. Each key in the dictionary is a node, and the corresponding value is a list of its child nodes. For example:
+
+                {
+                    "node1": ["child1", "child2"],
+                    "node2": ["child3",child4],
+                    "node3": []
+                    "node4": []
+                }
+        """
+
         # the ordering is to get the most of categories in a way that the parent is always before the child
         nodes = Category.objects.all()
         # normal dict but with default value as empty list []
-        getter = collections.defaultdict(list)
+        adjacency_list = collections.defaultdict(list)
         
         if not nodes:
             # initializing the first 2 buttons
@@ -29,21 +46,34 @@ class HomeView(View):
                 key = str(node.parent_id)
                     
                 # * storing childs for each parent
-                getter[key].append({"id":node.id,"type":node.type})
-                if not getter[str(node.id)]:
-                    getter[str(node.id)] = []
+                adjacency_list[key].append({"id":node.id,"type":node.type})
+                if not adjacency_list[str(node.id)]:
+                    adjacency_list[str(node.id)] = []
         
         
-        context = {"data":json.dumps(getter)}
+        context = {"data":json.dumps(adjacency_list)}
         
         
         # the request is from ajax so i will return json response
         if request.headers.get('Content-Type') == 'application/json':
-            return JsonResponse(getter)
+            return JsonResponse(adjacency_list)
         
         return render(request,'sw_task/home.html',context)
     
-    def post(self,request):
+    def post(self, request):
+        """
+        Summary:
+            Create sub-categories and associate them with a parent category.
+
+        Args:
+            request: A Django HTTP request object.
+
+        Returns:
+            JsonResponse: A JSON response indicating the status of the operation. Possible responses:
+
+            - {"status": "already clicked"}: If the category already has childs .
+            - {"status": "success"}: If sub-categories are successfully created and associated with the parent category.
+        """
         data = request.POST
         parent_id = data.get('parent')        
         # *just in case the client sent the request twice (i handled it in the frontend too)
@@ -57,3 +87,26 @@ class HomeView(View):
         sub_category_2.save()
         
         return JsonResponse({"status":"success"})
+    
+     # * so you can try my awesome work endlessly :D (Just kidding)
+    def delete(self, request):
+        """
+        Summary:
+            Delete all categories. and start over again.
+
+        Args:
+            request: A Django HTTP request object.
+
+        Returns:
+            JsonResponse: A JSON response indicating the status of the operation. Possible responses:
+
+            - {"status": "success"}: If all categories are successfully deleted.
+            - {"status": "error"}: If an error occurs during the deletion process.
+        """
+
+        try:
+            Category.objects.all().delete()
+        except:
+            return JsonResponse({"status": "error"})
+
+        return JsonResponse({"status": "success"})
