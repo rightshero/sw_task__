5bb19838e107304eb33fa17a335d4bb724a9508c
auto <auto>
2023-10-12 18:53:33 +0300

    dockerize the application

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:latest
+
+EXPOSE 8000
+
+WORKDIR /home/server
+
+COPY ./requirements.txt ./requirements.txt
+
+RUN pip3 install -r requirements.txt 
---
+++ wait_for_it.py
@@ -0,0 +1,36 @@
+import psycopg2
+import time
+from decouple import config
+
+def check_database():
+    db_params = {
+        'host': config('POSTGRES_HOST'),  
+        'port': config('POSTGRES_PORT'),
+        'user': config('POSTGRES_USER'),
+        'password': config('POSTGRES_PASSWORD'),
+        'database': config('POSTGRES_NAME'),
+    }
+
+    max_attempts = 3
+    delay = 5
+
+    for attempt in range(1, max_attempts + 1):
+        try:
+            conn = psycopg2.connect(**db_params)
+            conn.close()
+            print(f'Successfully connected to the database on attempt {attempt}.')
+            return True
+        except psycopg2.OperationalError as e:
+            print(f'Attempt {attempt}: Database not ready yet: {e}')
+            if attempt < max_attempts:
+                time.sleep(delay)
+    
+    print('Exceeded maximum attempts. Unable to connect to the database.')
+    return False
+
+if __name__ == '__main__':
+    if check_database():
+        print('Database is ready! Starting your Django API container...')
+        # Replace this line with the command to start your Django API container.
+    else:
+        print('Django API container will not start.')
