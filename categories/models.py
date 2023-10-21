d34fedad45233b9372df59511cc10db073205620
yaaa3ser <yasserissa1411@gmail.com>
2023-10-21 05:25:16 +0300

    add some tests to the categories app

--- categories/models.py
+++ categories/models.py
@@ -9,6 +9,4 @@ class Category(models.Model):
 
     def __str__(self):
         return "Category: " + str(self.id) + " Parent: " + str(self.parent_id) if self.parent_id else "Root Category"
-    # def __str__(self):
-    #     return  "Node: " + (str(self.pk)) + ("Child of: " + str(self.parent_id)) if self.parent_id else "Root Node"
-
+    
--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Category
+from .utils import get_adjacency_list, create_default_categories, create_sub_categories, check_if_child_exists
+import json
 
-# Create your tests here.
+class TestUtilsFunctions(TestCase):
+    
+    def test_create_default_categories(self):
+        # Test the create_default_categories function
+        create_default_categories()
+        self.assertEqual(Category.objects.count(), 2)
+
+    def test_create_sub_categories(self):
+        # Test the create_sub_categories function
+        category_a = Category.objects.create(name='Category A', parent=None)
+        create_sub_categories(category_a.id)
+        self.assertEqual(Category.objects.filter(parent_id=category_a.id).count(), 2)
+
+    def test_check_if_child_exists(self):
+        # Test the check_if_child_exists function
+        category_a = Category.objects.create(name='Category A', parent=None)
+        category_b = Category.objects.create(name='Category B', parent=category_a)
+        self.assertTrue(check_if_child_exists(category_a.id))
+        self.assertFalse(check_if_child_exists(category_b.id))
