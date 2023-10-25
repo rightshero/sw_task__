2173e05844009095f666fb407a25b42e6910a58d
3mawy <m.3mawyy@gmail.com>
2023-10-25 16:17:15 +0300

    add urls for the category views

--- categories/urls.py
+++ categories/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from categories.views import get_subcategories, category_selection_view
 
 urlpatterns = [
+    path('get_subcategories/<int:category_id>/', get_subcategories, name='get_subcategories'),
+    path('', category_selection_view, name='category_selection_view'),
 
 ]
