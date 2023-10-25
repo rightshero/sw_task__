6d7e963c1fc18e667c86b49d7cb906516f02bad7
3mawy <m.3mawyy@gmail.com>
2023-10-25 16:12:32 +0300

    handle get or create subcategories functions

---
+++ categories/views/category.py
@@ -0,0 +1,30 @@
+from django.http import JsonResponse
+from categories.models import Category
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def get_subcategories(request, category_id):
+    try:
+        parent_category = Category.objects.get(id=category_id)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'Parent category does not exist'}, status=400)
+
+    subcategories = Category.objects.filter(parent=parent_category)
+
+    if subcategories.exists():
+        data = [{'id': sub.id, 'name': sub.name} for sub in subcategories]
+    else:
+        data = create_subcategories(parent_category)
+
+    return JsonResponse(data, safe=False)
+
+
+def create_subcategories(parent_category):
+    subcategory_names = [f'SUB {parent_category.name} - 1', f'SUB {parent_category.name} - 2']
+
+    created_subcategories = []
+    for subcategory_name in subcategory_names:
+        subcategory = Category.objects.create(name=subcategory_name, parent=parent_category)
+        created_subcategories.append({'id': subcategory.id, 'name': subcategory.name})
+
+    return created_subcategories
