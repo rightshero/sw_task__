c361275df5b6c2136836c81bcc0158548ba3970d
3mawy <m.3mawyy@gmail.com>
2023-10-25 16:35:18 +0300

    admin config

--- categories/admin.py
+++ categories/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from django.core.management import call_command
+from categories.models import Category
+
+call_command('loaddata', 'initial_categories.json')
+admin.site.register(Category)
